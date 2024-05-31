9fb5bed0d4103676ed3e2463953e049d98b6e732
imrooteodoro <adelsonteodoro67@gmail.com>
2024-05-31 17:36:19 -0300

    feat: import routes

--- src/api/src/main.rs
+++ src/api/src/main.rs
@@ -1,22 +1,23 @@
-#[macro_use] extern crate rocket;
+#[macro_use]
+extern crate rocket;
 
-use rocket_dyn_templates::{Template, context};
-
-#[get("/")]
-fn index() -> Template {
-    Template::render("index", context! {name:"World"})
+mod routes {
+    pub mod index;
+    pub mod info;
+    pub mod sum;
 }
+use rocket_dyn_templates::{Template};
+use routes::index::*;
+use routes::sum::*;
+use routes::info::*;
 
-#[get("/<x>/<y>")]
-fn sum(x: u32, y: u32) -> Template {
-    Template::render("sum", context! {soma:(x + y).to_string()})
-}
+
 
 #[launch]
 fn rocket() -> _ {
     rocket::build()
         .mount("/", routes![index])
         .mount("/sum", routes![sum])
+        .mount("/info", routes![info])
         .attach(Template::fairing())
-       
 }

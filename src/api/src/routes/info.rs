c688ccb8da71ff78cf7ff569539ec79479fbebe1
imrooteodoro <adelsonteodoro67@gmail.com>
2024-05-31 17:35:41 -0300

    fix: public  struct info

--- src/api/src/routes/info.rs
+++ src/api/src/routes/info.rs
@@ -1,7 +1,8 @@
 use rocket_dyn_templates::{Template, context};
 
+
 #[get("/<name>/<age>")]
-fn info(name: &str, age: u8) -> Template {
+pub fn info(name: &str, age: u8) -> Template {
     Template::render("info", context! { nome: name, idade: age })
 }
 
